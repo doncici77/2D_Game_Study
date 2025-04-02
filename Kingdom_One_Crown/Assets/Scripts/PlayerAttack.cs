1d976e82600d65173b65cc75059c045275e60a16
doncici77 <pkh2956@naver.com>
2025-04-02 12:24:27 +0900

    공격 애니메이션 딜레이 추가

--- Kingdom_One_Crown/Assets/Scripts/PlayerAttack.cs
+++ Kingdom_One_Crown/Assets/Scripts/PlayerAttack.cs
@@ -1,22 +1,57 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
     private PlayerAnimation playerAnimation;
+    private Animator animator;
+
+    private bool isAttacking = false;
+
+    [Header("애니메이션 상태 이름")]
+    public string attackStateName = "Player_Attack";
 
     void Start()
     {
         playerAnimation = GetComponent<PlayerAnimation>();
+        animator = GetComponent<Animator>();
     }
 
     public void PerformAttack()
     {
-        if(playerAnimation != null)
+        if (Input.GetKeyDown(KeyCode.J))
         {
-            if(Input.GetKeyDown(KeyCode.J))
+            if (isAttacking)
+            {
+                return;
+            }
+
+            if (playerAnimation != null)
             {
                 playerAnimation.TriggerAttack();
             }
+
+            StartCoroutine(AttackColldownByAnimation());
         }
+    }
+
+    IEnumerator AttackColldownByAnimation()
+    {
+        isAttacking = true;
+
+        yield return null; // 다음 플레임까지 대기
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        if (stateInfo.IsName(attackStateName))
+        {
+            float animationLength = stateInfo.length;
+            yield return new WaitForSeconds(animationLength);
+        }
+        else
+        {
+            // 만약 다른 애니메이션 상태 이름이면 여기에 추가
+        }
+
+        isAttacking = false;
     }
 }
