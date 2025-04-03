8138871878e645922de4226e6bd3ca27b133445b
doncici77 <pkh2956@naver.com>
2025-04-03 11:53:38 +0900

    폰트 추가

--- 2D_Project/Assets/Scripts/GroundManager.cs
+++ 2D_Project/Assets/Scripts/GroundManager.cs
@@ -47,7 +47,7 @@ public class GroundManager : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if(collision.tag == "Player")
         {
             collision.transform.SetParent(gameObject.transform);
         }
@@ -55,7 +55,7 @@ public class GroundManager : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.tag == "Player" && collision.transform.parent.gameObject.activeInHierarchy)
         {
             collision.transform.SetParent(null);
         }
