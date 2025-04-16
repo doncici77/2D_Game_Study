e5cb36591199b44fb5007d434b4a8f83f349c868
doncici77 <pkh2956@naver.com>
2025-04-16 09:35:36 +0900

    플레이어 스탯 스크립트 추가

--- 2D_Project/Assets/Scripts/GameManager.cs
+++ 2D_Project/Assets/Scripts/GameManager.cs
@@ -9,6 +9,12 @@ public class GameManager : MonoBehaviour
 
     public Text coinText;
 
+    private const string COIN_KEY = "CoinCount";
+    private const string DAMAGE_KEY = "PlayerDamage";
+    private const string ATTACK_SPEED_KEY = "PlayerAttackSpeed";
+    private const string MOVE_SPEED_KEY = "PlayerMoveSpeed";
+    private const string HP_KEY = "PlayerHp";
+
     private void Awake()
     {
         if(instance == null)
@@ -26,8 +32,21 @@ public class GameManager : MonoBehaviour
     {
         coinCount += amount;
         coinText.text = "内牢 : " + coinCount.ToString();
+        SaveCoin();
         SoundManager.Instance.PlaySFX(SFXType.ItemGet);
-        PlayerPrefs.SetInt("Coin", coinCount);
+    }
+
+    public bool UseCoin(int amount)
+    {
+        if(coinCount >= amount)
+        {
+            coinCount -= amount;
+            SaveCoin();
+            return true;
+        }
+
+        Debug.Log("内牢 何练");
+        return false;
     }
 
     public int GetCoinCount()
@@ -35,10 +54,49 @@ public class GameManager : MonoBehaviour
         return coinCount;
     }
 
+    private void SaveCoin()
+    {
+        PlayerPrefs.SetInt(COIN_KEY, coinCount);
+        PlayerPrefs.Save();
+    }
+
+    private void LoadCoin()
+    {
+        coinCount = PlayerPrefs.GetInt(COIN_KEY, 0);
+    }
+
+    public void SavePlayerStats(PlayerStats stats)
+    {
+        PlayerPrefs.SetInt(DAMAGE_KEY, stats.damage);
+        PlayerPrefs.SetFloat(MOVE_SPEED_KEY, stats.moveSpeed);
+        PlayerPrefs.SetFloat(ATTACK_SPEED_KEY, stats.attackSpeed);
+        PlayerPrefs.SetInt(HP_KEY, stats.maxHp);
+    }
+
+    public void LoadPlayerStats(PlayerStats stats)
+    {
+        if(PlayerPrefs.HasKey(DAMAGE_KEY))
+        {
+            stats.damage = PlayerPrefs.GetInt(DAMAGE_KEY);
+        }
+        if (PlayerPrefs.HasKey(MOVE_SPEED_KEY))
+        {
+            stats.moveSpeed = PlayerPrefs.GetFloat(MOVE_SPEED_KEY);
+        }
+        if (PlayerPrefs.HasKey(ATTACK_SPEED_KEY))
+        {
+            stats.attackSpeed = PlayerPrefs.GetFloat(ATTACK_SPEED_KEY);
+        }
+        if (PlayerPrefs.HasKey(HP_KEY))
+        {
+            stats.maxHp = PlayerPrefs.GetInt(HP_KEY);
+        }
+
+    }
+
     public void ResetCoin()
     {
         coinCount = 0;
         coinText.text = "内牢 : " + coinCount.ToString();
-        PlayerPrefs.SetInt("Coin", coinCount);
     }
 }
---
+++ 2D_Project/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public static PlayerStats Instance;
+
+    [Header("플레이어 능력치")]
+    public int maxHp = 100;
+    public int currentHp;
+    public int damage = 10;
+    public float attackSpeed = 1.0f;
+    public float moveSpeed = 3.0f;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        currentHp = maxHp;
+    }
+
+    void Start()
+    {
+        GameManager.instance.LoadPlayerStats(this);
+    }
+
+    public void TakeDamage(int amount)
+    {
+        SoundManager.Instance.PlaySFX(SFXType.TakeDamage);
+        // 애니메이션 재생
+        currentHp -= amount;
+
+        if(currentHp <= 0)
+        {
+            // 죽음
+        }
+    }
+
+    public void Heal(int amount)
+    {
+        currentHp += amount;
+
+        if(currentHp > maxHp)
+        {
+            currentHp = maxHp;
+        }
+    }
+
+    public void Die()
+    {
+        // 예) GameOver
+    }
+
+    public int GetDamage()
+    {
+        return damage;
+    }
+
+    public float GetAttackSpeed()
+    {
+        return attackSpeed;
+    }
+
+    public void UpgradeDamage(int amount)
+    {
+        damage += amount;
+        GameManager.instance.SavePlayerStats(this);
+    }
+
+    public void UpgradeAttackSpeed(int amount)
+    {
+        attackSpeed += amount;
+        GameManager.instance.SavePlayerStats(this);
+    }
+
+    public void UpgradeHp(int amount)
+    {
+        maxHp += amount;
+        GameManager.instance.SavePlayerStats(this);
+    }
+
+    public void UpgradeMoveSpeed(int amount)
+    {
+        moveSpeed += amount;
+        GameManager.instance.SavePlayerStats(this);
+    }
+}
