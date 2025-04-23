e4af826356aa9c1a06b4eec23792b23a08d65907
doncici77 <pkh2956@naver.com>
2025-04-23 12:36:36 +0900

    클리어 맵으로 이동(페이드 효과 포함) 구현

--- 2D_Project/Assets/Scripts/Map/ClearPortal.cs
+++ 2D_Project/Assets/Scripts/Map/ClearPortal.cs
@@ -1,14 +1,22 @@
+using System.Collections;
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class ClearPortal : MonoBehaviour
 {
     private bool canUsePortal = false;
+    public Transform clearPortal;
+    public CinemachineConfiner2D confiner;
+    public Transform playerPos;
+    public GameObject camera;
+    public Collider2D cinePoly;
 
     private void Update()
     {
         if(canUsePortal && Input.GetKeyDown(KeyCode.W))
         {
             // 포탈 로직 
+            StartCoroutine(FadingPortal());
         }
     }
 
@@ -20,5 +28,42 @@ public class ClearPortal : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collision)
     {
         canUsePortal = false;
+    }
+
+    IEnumerator FadingPortal()
+    {
+        yield return StartCoroutine(FadeManager.instance.FadeIn());
+
+        yield return StartCoroutine(TeleportMap());
+
+        yield return StartCoroutine(FadeManager.instance.FadeOut());
+    }
+
+    IEnumerator TeleportMap()
+    {
+        // Cinemachine 설정 초기화
+        camera.GetComponent<CinemachineBrain>().enabled = false;
+
+        playerPos.position = clearPortal.position;
+
+        // Confiner 설정 초기화
+        confiner.BoundingShape2D = null;
+        confiner.InvalidateBoundingShapeCache();
+        confiner.gameObject.GetComponent<CinemachineCamera>().Target.TrackingTarget = null;
+
+        // 1 프레임 대기
+        yield return null;
+
+        // 새로운 위치에 맞게 재설정
+        confiner.gameObject.GetComponent<CinemachineCamera>().Target.TrackingTarget = playerPos;
+        confiner.BoundingShape2D = cinePoly;
+        confiner.InvalidateBoundingShapeCache();
+
+        // 카메라 재활성화
+        camera.GetComponent<CinemachineBrain>().enabled = true;
+
+        confiner.gameObject.SetActive(false);
+        yield return null;
+        confiner.gameObject.SetActive(true);
     }
 }
--- 2D_Project/Assets/Scripts/UI/FadeManager.cs
+++ 2D_Project/Assets/Scripts/UI/FadeManager.cs
@@ -1,16 +1,56 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FadeManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static FadeManager instance;
+
+    public Image panel;
+    private bool isFading = false;
+    public float fadeDuration = 1.0f;
+
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator FadeIn()
     {
-        
+        isFading = true;
+        panel.gameObject.SetActive(true);
+        yield return StartCoroutine(FadeImage(0, 1, fadeDuration));
+    }
+
+    public IEnumerator FadeOut()
+    {
+        yield return StartCoroutine(FadeImage(1, 0, fadeDuration));
+        isFading = false;
+        panel.gameObject.SetActive(false);
+    }
+
+    private IEnumerator FadeImage(float startAlpha, float endAlpha, float duration)
+    {
+        float elapsedTime = 0f;
+        Color panelColor = panel.color;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float newAlpha = Mathf.Lerp(startAlpha, endAlpha, elapsedTime / duration);
+            panelColor.a = newAlpha;
+            panel.color = panelColor;
+            yield return null;
+        }
+
+        panelColor.a = endAlpha;
+        panel.color = panelColor;
     }
 }
